e23b1efde62a969d57ed255363b1e4304f0aad99
MarvinScheffold <m@marvinscheffold.com>
2021-08-16 19:35:36 +0200

    added entry page scoop update tests

---
+++ src/routes/entry/tests/total-updates.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../options";
+
+test("update scoop subtotal when scoop selection changes", async () => {
+    render(<Options optionType={"scoop"} />);
+
+    // Make sure subtotal is 0.00 at beginning
+    const scoopsSubtotal = screen.getByText(/Scoops total/i);
+    expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+    // Update vanilla scoops to 1 and check subtotal
+    const vanillaInput = await screen.findByRole("spinbutton", {
+        name: /vanilla/i,
+    });
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, "1");
+    expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+    // Update chocolate scoops to 2 and check subtotal
+    const chocolateInput = await screen.findByRole("spinbutton", {
+        name: /chocolate/i,
+    });
+    userEvent.clear(chocolateInput);
+    userEvent.type(chocolateInput, "2");
+    expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
